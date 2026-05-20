67264ff0a3aaadd18f637eaaf0f76e64f0527887
takkoh <100661672+takkoh@users.noreply.github.com>
2026-05-20 14:04:59 -0700

    Fix database name casing in conexionBD.js

--- database/conexionBD.js
+++ database/conexionBD.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   port:     process.env.DB_PORT     || 3306,
   user:     process.env.DB_USER     || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'taller_carpinteriabd',
+  database: process.env.DB_NAME     || 'taller_carpinteriaBD',
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
