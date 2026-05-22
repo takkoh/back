20f070f7f29f86b279ceafa2695152ebf2d6ebfe
takkoh <pablo345,vel,g@gmail.com>
2026-05-21 22:15:57 -0700

    tiren paro

--- database/conexionBD.js
+++ database/conexionBD.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host:     process.env.DB_HOST     || 'localhost',
   port:     process.env.DB_PORT     || 3306,
-  user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'taller_carpinteriaBD',
+  user:     process.env.DB_USER     || 'app_user',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_NAME     || 'taller_carpinteriabd',
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
